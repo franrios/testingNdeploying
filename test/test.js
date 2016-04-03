67fb5b8ff7a8b3a6270c63b5f849afac4ba262b3
franrios <fcojriosbello@gmail.com>
2016-04-03 20:30:57 +0200

    Mocha tests added

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var request = require('request')
+var expect = require('Chai').expect
+
+const spawn = require('child_process').spawn
+
+describe('Server response', function () {
+  it('should return 200 and body must be the expected one', function (done) {
+    request.get('http://localhost:8080/', function (err, res, body) {
+      if (err) throw err
+      expect(res.statusCode).to.equal(200)
+      expect(res.body).to.equal('root path of the project!\n')
+      done()
+    })
+  })
+})
