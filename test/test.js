3671b415251576cd95c824fcc567390d8a852538
franrios <fcojriosbello@gmail.com>
2016-04-09 11:14:43 +0200

    spawn removed

--- test/test.js
+++ test/test.js
@@ -6,7 +6,6 @@ const spawn = require('child_process').spawn
  // var app = require('../app').getApp
 
 describe('Server response', function () {
-  var server = spawn('../index.js')
   it('should return 200 and body must be the expected one', function (done) {
     request.get('http://localhost:8080/', function (err, res, body) {
       if (err) throw err
