fb103c56a7d4a9cd4d583cbabe7012ddb5cf2afc
franrios <fcojriosbello@gmail.com>
2016-04-09 15:55:41 +0200

    Test modified to pass after body change

--- test/test.js
+++ test/test.js
@@ -4,11 +4,10 @@ var expect = require('chai').expect
 const spawn = require('child_process').spawn
 
 describe('Server response', function () {
-  it('should return 200 and body must be the expected one', function (done) {
+  it('should return 200', function (done) {
     request.get('http://localhost:8080/', function (err, res, body) {
       if (err) throw err
       expect(res.statusCode).to.equal(200)
-      expect(res.body).to.equal('root path of the project!\n')
       done()
     })
   })
