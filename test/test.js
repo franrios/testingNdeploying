15a3a5a405142485943178236446b195f04eca29
franrios <fcojriosbello@gmail.com>
2016-04-09 12:30:32 +0200

    simplified tests

--- test/test.js
+++ test/test.js
@@ -3,8 +3,6 @@ var expect = require('chai').expect
 
 const spawn = require('child_process').spawn
 
- // var app = require('../app').getApp
-
 describe('Server response', function () {
   it('should return 200 and body must be the expected one', function (done) {
     request.get('http://localhost:8080/', function (err, res, body) {
@@ -12,7 +10,6 @@ describe('Server response', function () {
       expect(res.statusCode).to.equal(200)
       expect(res.body).to.equal('root path of the project!\n')
       done()
-      server.kill()
     })
   })
 })
