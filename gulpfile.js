91f67ab1f7ca07ccf2c94828acaddb7b8a330e74
franrios <fcojriosbello@gmail.com>
2016-04-18 19:21:30 +0200

    syntax improved

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,16 @@ var bg = require('gulp-bg')
 var bgstart
 gulp.task('start', bgstart = bg('node', './index.js'))
 
+function terminate () {
+  bgstart.setCallback(function () { process.exit(0) })
+  bgstart.stop(0)
+}
+
 gulp.task('test', ['start'], function () {
   return gulp.src('./test/test.js', {read: false})
   .pipe(mocha({reporter: 'nyan'}))
-  .once('end', function () {
-    bgstart.setCallback(function () { process.exit(0) })
-    bgstart.stop(0)
-  })
-  .once('error', function () {
-    bgstart.setCallback(function () { process.exit(0) })
-    bgstart.stop(0)
-  })
+  .once('end', terminate)
+  .once('error', terminate)
 })
 
 gulp.task('default', ['start', 'test'])
